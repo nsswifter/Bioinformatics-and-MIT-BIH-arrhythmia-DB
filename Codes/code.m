fa1192c6649476da0d5a0131b6bfe31194dbe273
Mehdi Karami <90229452+mehdi2003karami@users.noreply.github.com>
2023-05-24 12:51:42 +0330

    Update code.m

--- Codes/code.m
+++ Codes/code.m
@@ -80,33 +80,90 @@ ylabel('Original Signal-ECG(mv)');
 %% Finding Peaks
 temp = 1;
 for index = 1: length(UD) - 1
-    if (UD(index) ~= UD(index + 1));
+    if (UD(index) ~= UD(index + 1))
         index_peak(temp) = index;
         temp = temp + 1;
     end
 end
 
 for index = 1: length(index_peak)
-        s4_peak_R = subSignal3(index_peak);
-        t4_peak_R = subTime3(index_peak);
+        signal4_peak_R = subSignal3(index_peak);
+        time4_peak_R = subTime3(index_peak);
 end
+
+%% Drawing Peaks
 subplot(3, 1, 1)
 hold on
-plot(t4_peak_R, s4_peak_R, 'g*', 'LineWidth', 2);
+plot(time4_peak_R, signal4_peak_R, 'g*', 'LineWidth', 2);
 
-%%
+%% Drawing UD
 subplot(3, 1, 2)
 stairs(subTime3, UD(1 : length(subTime3)), 'LineWidth', 1);
 axis([0, 3, -0.5, 1.2]);
+xlabel('Time(Sec)');
 ylabel('UD');
-xlabel('Time*(Sec)');
 
-%%
+%% Drawing Token
 subplot(3, 1, 3)
 token = ones(1, length(subTime3));
 stem(subTime3, token, 'LineWidth', 1.4)
 axis([0, 3, -0.5, 1.2]);
-ylabel('Time*(Sec)');
-xlabel('Token');
+xlabel('Time(Sec)');
+ylabel('Token');
+
+%% Setting Duration
+j = 1;
+n = 4;
+W = 9;
+
+Lower = floor(W / 2);
+Upper = floor(W / 2) - n + j;
+
+% Lower = -4; Upper = 2;
+dp_Dti(1) = 0;
+
+for index = 1:length(Dti) 
+    dp_Dti(index + 1) = dp_Dti(index) + Dti(index);
+end
+
+for index = 1:length(index_peak)
+    down(index) = max(1, index_peak(index) - Lower);
+    up(index) = min(length(dp_Dti), index_peak(index) + Upper);
+    duration(index_peak(index)) = dp_Dti(up(index)) - dp_Dti(down(index));
+    durationPlus(index) = dp_Dti(up(index)) - dp_Dti(down(index));
+end
+
+%% Adaptive Thresholding = TH1
+coeff1 = 0.25;
+coeff2 = 0.25;
+
+SP(1) = 0.1633;
+NP(1) = 0.15;
+
+for index = 1:length(index_peak) 
+    SP(index + 1) = SP(index) - (coeff1 * (SP(index) - durationPlus(index)));
+    NP(index + 1) = NP(index) - (coeff1 * (NP(index) - durationPlus(index)));
+    TH1(index_peak(index)) = SP(index) - (coeff2 * (NP(index) - SP(index)));
+    TH1Plus(index) = NP(index) - (coeff2 * (NP(index) - SP(index)));
+end
+
+%% Detect R_Peak with TH1
+temp = 1;
+for index = 1:length(index_peak)
+    if TH1(index_peak(index)) > duration(index_peak(index))
+        index_R_peak(temp) = index_peak(index);
+        temp = temp + 1;
+    end
+end
+
+for index = 1:length(index_R_peak)
+    signal5_R_peak = subSignal3(index_R_peak);
+    time5_R_peak = subTime3(index_R_peak);
+end
+
+%% Drawing R_Peak
+subplot(3, 1, 1)
+hold on
+plot(time5_R_peak, signal5_R_peak, 'bo', 'LineWidth', 2);
 
 % ---------------------------------------------------------------
